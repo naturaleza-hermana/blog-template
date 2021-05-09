c1ee906572355b24d95529154eccd46da8f9a414
naturaleza <contacto.naturalezahermana@example.com>
2021-05-09 17:47:53 -0500

    new design

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,34 +13,12 @@ export default function Layout({ children }: Props) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#fff" />
+        <link rel="stylesheet" href="https://tailwindcomponents.com/css/component.nature.css" />
+        <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,800|Montserrat:600|Alegreya+Sans:500&amp;display=swap" rel="stylesheet"/>
       </Head>
-      <nav>
-        <Navigation />
-      </nav>
+      
       <main>{children}</main>
-      <style jsx>
-        {`
-          .root {
-            display: block;
-            padding: 4rem 0;
-            box-sizing: border-box;
-            height: 100%;
-          }
-          main {
-            display: flex;
-            min-height: 100%;
-          }
-          @media (min-width: 769px) {
-            .root {
-              display: flex;
-              flex: 1 0 auto;
-            }
-            main {
-              flex: 1 0 auto;
-            }
-          }
-        `}
-      </style>
+      
     </div>
   );
 }
